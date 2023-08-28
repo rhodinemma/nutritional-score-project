7fc69b4769fe3d21697b8b12e1313595dd7fdc9f
rhodinemma <rhodinemma10@gmail.com>
2023-08-28 14:05:18 +0300

    feat: define nutritional score and other types

--- nutritionalscore.go
+++ nutritionalscore.go
@@ -1,1 +1,31 @@
 package main
+
+type ScoreType int
+
+const (
+	Food ScoreType = iota
+	Beverage
+	Water
+	Cheese
+)
+
+type NutritionalScore struct {
+	Value     int
+	Positive  int
+	Negative  int
+	ScoreType ScoreType
+}
+
+type EnergyKJ float64
+
+type SugarGram float64
+
+type SaturatedFattyAcids float64
+
+type SodiumMilligram float64
+
+type FruitsPercent float64
+
+type FibreGram float64
+
+type ProteinGram float64
