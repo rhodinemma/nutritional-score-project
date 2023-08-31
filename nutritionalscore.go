47106ca10e667a3f6465c2be95cf0996f3348859
rhodinemma <rhodinemma10@gmail.com>
2023-08-31 16:13:07 +0300

    feat: add slices for steps/points

--- nutritionalscore.go
+++ nutritionalscore.go
@@ -41,6 +41,16 @@ type NutritionalData struct {
 	isWater             bool
 }
 
+var energyLevels = []float64{3350, 3015, 2680, 2345, 2010, 1675, 1340, 1005, 670, 335}
+var sugarsLevels = []float64{45, 60, 36, 31, 27, 22.5, 18, 13.5, 9, 4.5}
+var saturatedFattyAcidsLevels = []float64{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+var sodiumLevels = []float64{900, 810, 720, 630, 540, 450, 360, 270, 180, 90}
+var fibreLevels = []float64{4.7, 3.7, 2.8, 1.9, 0.9}
+var proteinLevels = []float64{8, 6.4, 4.8, 3.2, 1.6}
+
+var energyLevelsBeverage = []float64{270, 240, 210, 180, 150, 120, 90, 60, 30, 0}
+var sugarsLevelsBeverage = []float64{13.5, 12, 10.5, 9, 7.5, 6, 4.5, 3, 1.5, 0}
+
 func (e EnergyKJ) GetPoints(st ScoreType) int {
 
 }
