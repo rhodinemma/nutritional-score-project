c74d58a508ecd063981bfd4cd9379a32526c5c38
rhodinemma <rhodinemma10@gmail.com>
2023-08-31 17:23:54 +0300

    feat: add fruit getPoints functionality

--- nutritionalscore.go
+++ nutritionalscore.go
@@ -74,7 +74,26 @@ func (s SodiumMilligram) GetPoints(st ScoreType) int {
 }
 
 func (f FruitsPercent) GetPoints(st ScoreType) int {
+	if st == Beverage {
+		if f > 80 {
+			return 10
+		} else if f > 60 {
+			return 4
+		} else if f > 40 {
+			return 2
+		}
+		return 0
+	}
 
+	if f > 80 {
+		return 5
+	} else if f > 60 {
+		return 2
+	} else if f > 40 {
+		return 1
+	}
+
+	return 0
 }
 
 func (f FibreGram) GetPoints(st ScoreType) int {
@@ -93,7 +112,7 @@ func SodiumFromSalt(saltMg float64) SodiumMilligram {
 	return SodiumMilligram(saltMg / 2.5)
 }
 
-func GetNutritionalScoren(n NutritionalData, st ScoreType) NutritionalScore {
+func GetNutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
 	value := 0
 	positive := 0
 	negative := 0
@@ -112,6 +131,10 @@ func GetNutritionalScoren(n NutritionalData, st ScoreType) NutritionalScore {
 		Negative:  negative,
 		ScoreType: st,
 	}
+}
+
+func (ns NutritionalScore) GetNutriScore() string {
+
 }
 
 func getPointsFromRange(v float64, steps []float64) int {
